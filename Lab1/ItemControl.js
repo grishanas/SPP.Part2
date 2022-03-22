beb4a816332502bd1da00381bec5c6ce0fc8a558
grishanas <80056655+grishanas@users.noreply.github.com>
2022-03-22 10:58:53 +0300

    Initial commit

---
+++ Lab1/ItemControl.js
@@ -0,0 +1,122 @@
+const fs = require('fs');  
+const { request } = require('http');
+const path = require('path')
+const DataPath = 'Data/data.json';
+
+const util = require('util')
+const { pipeline } = require('stream');
+const { brotliDecompress } = require('zlib');
+const pump = util.promisify(pipeline)
+
+const fastify = require('fastify')({ logger: true })
+fastify.register(require('fastify-multipart'))
+
+function readFile(filePath){
+    try{
+       let Data = fs.readFileSync(path.resolve(__dirname,filePath));
+        //console.log(Data);
+        return Data;
+    }catch(e){
+        
+        console.log(e);
+    }
+
+}
+
+
+
+function SaveNewUpdate(filePath,data)
+{
+    try{
+        console.log(JSON.stringify(data));
+        fs.writeFile(path.resolve(__dirname,filePath),JSON.stringify(data),(err)=>{
+            if(err)
+                console.log(err);
+        });
+    }catch(e){
+        console.log(e);
+
+    }
+}
+
+
+const GetAllItems = async (Request,reply)=>{
+    reply.body = readFile(DataPath);
+    return reply;
+}
+
+const GetItemWithId= async (Request,Reply)=>{
+    let id = Number(Request.params.id);
+
+}
+
+const AddItem = async (Request,Reply)=>{
+
+    let data = readFile(DataPath);
+    let json = JSON.parse(data);
+    let max=0;
+    try{
+        json.forEach(element => {
+            if(element.id>max)
+                max=element.id;
+        });
+        max++;
+    }catch(e){
+        console.log(e);
+    }
+
+    let buffer = await Request.file();
+    
+    let body = new Object();
+    if(buffer.fields.TaskName.value==='undefined')
+        body.TaskName="";
+    else
+        body.TaskName = buffer.fields.TaskName.value;
+
+    if(buffer.fields.DateTime.value==='undefined')
+        body.DateTime="";
+    else
+        body.DateTime = buffer.fields.DateTime.value;
+    
+
+    try{
+    if(buffer.fields['Status']==void (0) )
+        body.Status="";
+    else
+        body.Status = buffer.fields['Status'].value;
+        
+    }
+    catch(e){
+        body.Status="";
+    }
+
+    if (buffer.filename!='')
+    {
+        body.file=buffer.filename;
+        await pump(buffer.file, fs.createWriteStream(path.join(__dirname,'Data',buffer.filename)));
+    }
+    else
+        body.file='';
+    
+    let newItem={
+        id : max,
+        body:body,
+    }
+    json.push(newItem);
+    SaveNewUpdate(DataPath,json)
+    
+    return newItem;
+}
+
+const ReadAllItem = async()=>{
+    return JSON.parse(readFile(DataPath));
+
+}
+
+
+module.exports = {
+    GetAllItems,
+    GetItemWithId,
+    AddItem,
+    ReadAllItem,
+}
---
+++ Lab1/server.js
@@ -0,0 +1,164 @@
+const fastify = require('fastify')({ logger: true })
+const path= require('path');
+const ApiControl = require('../Lab1/ItemControl');
+const Handlebars = require('handlebars');
+const { Exception } = require('handlebars');
+const fs = require('fs');  
+const url = require('url');
+const { request } = require('http');
+
+
+const data = [];
+
+
+fastify.register(require('fastify-formbody'))
+fastify.register(require("fastify-multipart"))
+
+fastify.register(require("point-of-view"), {
+  engine: {
+    handlebars: require("handlebars"),
+  },
+  root: path.join(__dirname,'/View'),
+  includeViewExtension:true,
+});
+
+Handlebars.registerHelper('GetStatus',(json)=>{
+  try{
+  if(json['body']['Status']===undefined)
+    throw new Exception();
+    let type="";
+  
+    switch(json['body']['Status'])
+    {
+      case '1':
+        {
+      
+          type="Выполнена"
+          break;
+        }
+      case '2':
+        {
+          type="Отменено"
+          break;
+        }
+      case '3':
+        {
+          type="Выполняется"
+          break;
+        }  
+    }
+    
+  return new Handlebars.SafeString(type)
+  }
+  catch(e)
+  {
+
+  }
+})
+
+Handlebars.registerHelper('GetUrl',(json,option)=>{
+  try{
+    if(json['body']['file']=='')
+      throw new Exception();
+  return new Handlebars.SafeString("<a download href=\"Data/" + json["body"]["file"] +"\" >" + "скачать файл: "+json["body"]["file"]+"</a>") 
+  }
+  catch(e)
+  {
+    return new Handlebars.SafeString("<p> Файл не найден </p>");
+  }
+});
+
+async function IndexGet (request, reply){
+
+  let data = await ApiControl.ReadAllItem();
+  let url_ = (url.parse(request.url,true));
+
+
+  try{
+    if(url_.query['Column']=='id')
+      if(url_.query['Course']=='UP')
+      {
+        console.log('UP');
+        data.sort(function (a,b) {
+          return a['id']-b['id'];
+        });
+      } 
+      else
+      data.sort((a,b)=>{
+        return b['id']-a['id'];
+      });
+      if(url_.query['Column']=='Status')
+        if(url_.query['Course']=='UP')
+        {
+          data.sort((a,b)=>{
+            return a['body']['Status']-b['body']['Status'];
+          })
+        }else{
+          data.sort((a,b)=>
+          {
+            return b['body']['Status']-a['body']['Status'];
+          })
+        }
+  }
+  finally
+  {
+
+  reply.view('index', { tabel: (data)} );
+  }
+
+}
+
+async function IndexPost(request,reply){
+  await ApiControl.AddItem(request,reply);
+  reply.redirect('/');
+}
+
+async function DeleteData(request,reply){
+  console.log("new Delete");
+}
+
+const routers =[{
+  method: 'POST',
+  url:'/',
+  handler: IndexPost,
+  },
+  {
+    method:'GET',
+    url:'/',
+    handler: IndexGet,
+  },
+  {
+    method:'DELETE',
+    url:'/',
+    handler:DeleteData,
+
+  }
+]
+
+
+fastify.get('/*',async (request,reply)=>{
+  reply.type('text/css');
+  reply.send(fs.readFileSync(path.join(__dirname,request.url)));
+})
+
+
+fastify.get('/Data/*',async (request,reply)=>{
+  reply.send(fs.readFileSync(path.join(__dirname,request.url)));
+})
+
+
+routers.forEach((route,index)=>{
+  fastify.route(route);
+
+})
+
+
+const start = async () => {
+  try {
+    await fastify.listen(3000)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
